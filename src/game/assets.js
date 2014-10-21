16f2409cd565c4d89f7a5272a37d8c9b4d0ad0e3
simo2703 <simo2703@vandals.uidaho.edu>
2014-10-21 15:09:48 -0700

    Menu Functionality
    added code for basic menu functionality...currently all buttons take you
    to the game

--- src/game/assets.js
+++ src/game/assets.js
@@ -8,6 +8,12 @@ game.addAsset('images/ballchiphome.png', 'chip-home');
 game.addAsset('images/ballchipaway.png', 'chip-away');
 game.addAsset('images/kickshotbig.png', 'title');
 
+// Menu Images
+game.addAsset('images/background.png', 'menu');
+game.addAsset('images/playButton.png', 'playButton');
+game.addAsset('images/questionButton.png', 'questionButton');
+game.addAsset('images/webLincButton.png', 'webLincButton');
+
 game.addAsset('images/dice1.png', 'dice1');
 game.addAsset('images/dice2.png', 'dice2');
 game.addAsset('images/dice3.png', 'dice3');
