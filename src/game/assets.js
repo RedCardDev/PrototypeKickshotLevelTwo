40be8874cc5863a9af27f30d579e9e95357b7a44
simo2703 <simo2703@vandals.uidaho.edu>
2014-11-18 12:41:16 -0800

    button and menu view tweening
    need to make as menu sprite instead of separate scene

--- src/game/assets.js
+++ src/game/assets.js
@@ -1,7 +1,6 @@
 game.module(
-    'game.assets'
+'game.assets'
 ).body(function() {
-
 game.addAsset('logo.png');
 game.addAsset('images/blank_field.png', 'field');
 game.addAsset('images/ballchiphome.png', 'chip-home');
@@ -11,8 +10,18 @@ game.addAsset('images/kickshotbig.png', 'title');
 // Menu Images
 game.addAsset('images/background.png', 'menu');
 game.addAsset('images/playButton.png', 'playButton');
-game.addAsset('images/questionButton.png', 'questionButton');
+//game.addAsset('images/questionButton.png', 'questionButton');
 game.addAsset('images/webLincButton.png', 'webLincButton');
+game.addAsset('images/background.png','help');
+game.addAsset('images/backButton.png', 'backButton');
+
+//Card Pick View
+game.addAsset('images/CardPickDesignSingle.png','cardPickBackground');
+game.addAsset('images/menuButton.png', 'menuButton');
+game.addAsset('images/MainViewButtonTransparent.png', 'mainViewButton');
+game.addAsset('images/SkipButtonTransparent.png', 'skipButton');
+game.addAsset('images/tradeCardButtonTransparent.png', 'tradeButton');
+game.addAsset('images/UseCardButtonTransparent.png', 'useCardButton');
 
 game.addAsset('images/dice1.png', 'dice1');
 game.addAsset('images/dice2.png', 'dice2');
@@ -20,5 +29,4 @@ game.addAsset('images/dice3.png', 'dice3');
 game.addAsset('images/dice4.png', 'dice4');
 game.addAsset('images/dice5.png', 'dice5');
 game.addAsset('images/dice6.png', 'dice6');
-
 });
